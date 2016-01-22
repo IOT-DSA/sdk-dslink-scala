d677a6fac2f694bf61c83a47075296309d6694be
Vlad Orzhekhovskiy <vlad@uralian.com>
2016-01-22 12:20:07 -0500

    Implement Action utilities

--- src/main/scala/org/dsa/iot/package.scala
+++ src/main/scala/org/dsa/iot/package.scala
@@ -1,11 +1,13 @@
 package org.dsa
 
-import scala.collection.JavaConverters.{ asScalaSetConverter, mapAsScalaMapConverter }
+import scala.collection.JavaConverters.{ asScalaSetConverter, mapAsScalaMapConverter, seqAsJavaListConverter }
 
+import org.dsa.iot.Having
 import org.dsa.iot.dslink.node.{ Node, NodeBuilder, Permission, Writable }
-import org.dsa.iot.dslink.node.actions.{ Action, ActionResult }
+import org.dsa.iot.dslink.node.actions.{ Action, ActionResult, EditorType, Parameter, ResultType }
 import org.dsa.iot.dslink.node.value.{ Value, ValueType }
 import org.dsa.iot.dslink.util.handler.Handler
+import org.dsa.iot.dslink.util.json.JsonObject
 import org.dsa.iot.util.ValueUtils
 
 /**
@@ -79,6 +81,87 @@ package object iot extends ValueUtils {
       action(new Action(permission, new Handler[ActionResult] {
         def handle(event: ActionResult) = handler(event)
       }))
+  }
+
+  /**
+   * Creates a new action.
+   */
+  def action(handler: ActionHandler,
+             parameters: Iterable[Parameter] = Nil,
+             results: Iterable[Parameter] = Nil,
+             permission: Permission = Permission.READ,
+             resultType: ResultType = ResultType.VALUES,
+             hidden: Boolean = false): Action = {
+    val a = new Action(permission, new Handler[ActionResult] {
+      def handle(event: ActionResult) = handler(event)
+    })
+    parameters foreach a.addParameter
+    results foreach a.addResult
+    a.setResultType(resultType)
+    a.setHidden(hidden)
+    a
+  }
+
+  /**
+   * Extension to Action which allows to retrieve parameters and results.
+   */
+  implicit class RichAction(val action: Action) extends AnyVal {
+    def parameters = jsonArrayToList(action.getParams)
+    def results = jsonArrayToList(action.getColumns)
+  }
+
+  /**
+   * Extension to Parameter which provides fluent syntax for building parameters.
+   */
+  implicit class RichParameter(val param: Parameter) extends AnyVal {
+    def default(value: Any) = param having (_.setDefaultValue(anyToValue(value)))
+    def description(value: String) = param having (_.setDescription(value))
+    def editorType(value: EditorType) = param having (_.setEditorType(value))
+    def placeHolder(value: String) = param having (_.setPlaceHolder(value))
+    def meta(value: JsonObject): Parameter = param having (_.setMetaData(value))
+    def meta(value: Map[String, Any]): Parameter = meta(mapToJsonObject(value))
+  }
+
+  /**
+   * Creates a new ENUM value type from Scala enumeration.
+   */
+  def ENUMS(enum: Enumeration): ValueType = ENUMS(enum.values.map(_.toString).toSeq: _*)
+
+  /**
+   * Creates a new ENUM value type from a collection of strings.
+   */
+  def ENUMS(values: String*): ValueType = ValueType.makeEnum(values.asJava)
+
+  /**
+   * Extension to ValueType which allows creating new parameters with the given type and name.
+   */
+  implicit class RichValueType(val vt: ValueType) extends AnyVal {
+    def apply(name: String) = new Parameter(name, vt)
+  }
+
+  /**
+   * Extension to ActionResult which allows to extract parameters and results.
+   */
+  implicit class RichActionResult(val event: ActionResult) extends AnyVal {
+
+    /**
+     * Extracts a parameter of the specified type, optionally checking if it is valid.
+     * @param T the parameter type. An implicit extractor Value=>T must exist for type T.
+     * @param name parameter name.
+     * @param check function which checks if the parameter value is valid. If it returns `false`,
+     * `IllegalArgumentException` is thrown.
+     * @param msg message to pass to the exception, if the value fails validation.
+     * @return the parameter value.
+     * @throws IllegalArgumentException if the parameter value fails validation.
+     */
+    def getParam[T](name: String, check: T => Boolean = (_: T) => true, msg: String = "")(implicit ex: Value => T): T = {
+      val value = ex(event.getParameter(name))
+
+      if (!check(value))
+        throw new IllegalArgumentException(msg)
+      else
+        value
+    }
   }
 
   /**
---
+++ src/test/scala/org/dsa/iot/ActionSupportSpec.scala
@@ -0,0 +1,131 @@
+package org.dsa.iot
+
+import scala.collection.JavaConverters.collectionAsScalaIterableConverter
+
+import org.dsa.iot.dslink.node.{ Node, Permission }
+import org.dsa.iot.dslink.node.actions.{ ActionResult, EditorType, Parameter, ResultType }
+import org.dsa.iot.dslink.node.value.ValueType
+
+/**
+ * Action utilities test suite.
+ */
+class ActionSupportSpec extends AbstractSpec {
+
+  "action" should {
+    "create new actions with default attributes" in {
+      var result = ""
+      val handler = (event: ActionResult) => { result = "done" }
+      val a = action(handler)
+      jsonArrayToList(a.getParams) shouldBe empty
+      jsonArrayToList(a.getColumns) shouldBe empty
+      a.getPermission shouldBe Permission.READ
+      a.getResultType shouldBe ResultType.VALUES
+      a.isHidden() shouldBe false
+      a.invoke(null)
+      result shouldBe "done"
+    }
+    "create new actions with all attributes" in {
+      for {
+        perm <- Permission.values
+        rt <- ResultType.values
+        hid <- List(false, true)
+        a = action(
+          handler = (event: ActionResult) => {},
+          permission = perm,
+          resultType = rt,
+          hidden = hid)
+      } {
+        a.getPermission shouldBe perm
+        a.getResultType shouldBe rt
+        a.isHidden shouldBe hid
+      }
+    }
+    "create new actions with parameters and results" in {
+      val a = action(
+        parameters = List(new Parameter("a", ValueType.STRING), new Parameter("b", ValueType.NUMBER)),
+        results = List(new Parameter("x", ValueType.BOOL), new Parameter("y", ValueType.BINARY)),
+        handler = (event: ActionResult) => {})
+      a.parameters shouldBe List(Map("name" -> "a", "type" -> "string"), Map("name" -> "b", "type" -> "number"))
+      a.results shouldBe List(Map("name" -> "x", "type" -> "bool"), Map("name" -> "y", "type" -> "binary"))
+    }
+  }
+
+  "RichParameter" should {
+    "add parameter attributes" in {
+      val param = new Parameter("abc", ValueType.STRING)
+      param default "100" description "ABC" editorType EditorType.PASSWORD placeHolder "a-b-c" meta Map("x" -> "y")
+
+      param.getName shouldBe "abc"
+      param.getType shouldBe ValueType.STRING
+      param.getDefault shouldBe anyToValue("100")
+      param.getDescription shouldBe "ABC"
+      param.getEditorType shouldBe EditorType.PASSWORD
+      param.getPlaceHolder shouldBe "a-b-c"
+      jsonObjectToMap(param.getMetaData) shouldBe Map("x" -> "y")
+    }
+  }
+
+  "RichValueType" should {
+    import ValueType._
+
+    def checkParam(param: Parameter, pName: String, pType: ValueType) =
+      (param.getName, param.getType) shouldBe Tuple2(pName, pType)
+
+    "create fixed type parameters" in {
+      checkParam(STRING("a"), "a", STRING)
+      checkParam(NUMBER("a"), "a", NUMBER)
+      checkParam(BOOL("a"), "a", BOOL)
+      checkParam(BINARY("a"), "a", BINARY)
+      checkParam(ARRAY("a"), "a", ARRAY)
+      checkParam(MAP("a"), "a", MAP)
+    }
+    "create dynamic parameters" in {
+      checkParam(DYNAMIC("a"), "a", DYNAMIC)
+    }
+    "create enumerated parameters" in {
+      val p1 = ENUMS("x", "y", "z")("a")
+      p1.getName shouldBe "a"
+      p1.getType.getRawName shouldBe "enum"
+      p1.getType.getEnums.asScala.toList shouldBe List("x", "y", "z")
+
+      object MyEnum extends Enumeration {
+        type MyEnum = Value
+        val E1, E2, E3 = Value
+      }
+
+      val p2 = ENUMS(MyEnum)("b")
+      p2.getName shouldBe "b"
+      p2.getType.getRawName shouldBe "enum"
+      p2.getType.getEnums.asScala.toList shouldBe List("E1", "E2", "E3")
+    }
+  }
+
+  "RichActionResult" should {
+    val node = new Node("a", null, null)
+    val json = mapToJsonObject("params" -> mapToJsonObject("a" -> "abc", "x" -> 123, "y" -> true,
+      "z" -> "1234".getBytes, "v" -> listToJsonArray(1, 2, 3), "w" -> mapToJsonObject("c" -> "C")))
+    val ar = new ActionResult(node, json)
+
+    "extract parameters without validation" in {
+      ar.getParam[String]("a") shouldBe "abc"
+      ar.getParam[Number]("x") shouldBe 123
+      ar.getParam[Boolean]("y") shouldBe true
+      ar.getParam[Array[Byte]]("z") shouldBe "1234".getBytes
+      ar.getParam[List[Any]]("v") shouldBe List(1, 2, 3)
+      ar.getParam[Map[String, Any]]("w") shouldBe Map("c" -> "C")
+    }
+
+    "extract numeric parameters with concrete types" in {
+      ar.getParam[Int]("x") shouldBe 123
+      ar.getParam[Double]("x") shouldBe 123.0
+      a[NullPointerException] should be thrownBy ar.getParam[Int]("xxx")
+    }
+
+    "extract parameters with validation" in {
+      ar.getParam[Number]("x", _.doubleValue > 100) shouldBe 123
+      an[IllegalArgumentException] should be thrownBy ar.getParam[Int]("x", _ < 100)
+      ar.getParam[String]("a", _.size == 3) shouldBe "abc"
+      an[IllegalArgumentException] should be thrownBy ar.getParam[String]("a", _.size == 4)
+    }
+  }
+}
