0697f019716905be7363c99d91c72d00e0a68726
Vlad Orzhekhovskiy <vlad@uralian.com>
2016-08-09 14:33:28 -0400

    Fix Value-any conversion issue for containers

--- src/main/scala/org/dsa/iot/util/ValueUtils.scala
+++ src/main/scala/org/dsa/iot/util/ValueUtils.scala
@@ -46,21 +46,23 @@ trait ValueUtils {
    */
   def anyToValue(value: Any): Value = value match {
     case null                => null
+    case x: Value            => x
     case x: java.lang.Number => numberToValue(x)
     case x: Boolean          => booleanToValue(x)
     case x: String           => stringToValue(x)
     case x: Array[Byte]      => binaryToValue(x)
-    case x: Map[_, _]        => mapToValue(x.asInstanceOf[Map[String, _]]) 
-    case x: List[_]          => listToValue(x)
+    case x: Map[_, _]        => mapToValue(x.asInstanceOf[Map[String, _]])
+    case x: Seq[_]           => listToValue(x)
     case x @ _               => new Value(x.toString)
   }
 
   /**
-   * Converts a scala List[Any] instance into a JsonArray.
+   * Converts a scala Seq[Any] instance into a JsonArray.
    */
-  def listToJsonArray(ls: List[_]): JsonArray = {
+  def listToJsonArray(ls: Seq[_]): JsonArray = {
     val elements = ls map {
-      case x: List[_]   => listToJsonArray(x)
+      case x: Value     => valueToAny(x)
+      case x: Seq[_]    => listToJsonArray(x)
       case x: Map[_, _] => mapToJsonObject(x.asInstanceOf[Map[String, Any]])
       case x            => x
     }
@@ -70,18 +72,19 @@ trait ValueUtils {
   /**
    * Converts scala values into a JsonArray.
    */
-  def listToJsonArray(values: Any*): JsonArray = listToJsonArray(values.toList)
+  def jsonArray(values: Any*): JsonArray = listToJsonArray(values.toList)
 
   /**
    * Converts a scala Map[String, Any] instance into a JsonObject.
    */
   def mapToJsonObject(mp: Map[String, _]): JsonObject = {
     val elements = mp.mapValues {
-      case x: List[_]   => listToJsonArray(x)
+      case x: Value     => valueToAny(x).asInstanceOf[Object]
+      case x: Seq[_]    => listToJsonArray(x)
       case x: Map[_, _] => mapToJsonObject(x.asInstanceOf[Map[String, Any]])
       case x            => x.asInstanceOf[Object]
     }
-    // due to issues with mutability, have to do it the log way instead of elements.toJava
+    // due to issues with mutability, have to do it the long way instead of elements.toJava
     val m = new java.util.HashMap[String, Object]
     elements foreach {
       case (key, value) => m.put(key, value)
@@ -92,7 +95,7 @@ trait ValueUtils {
   /**
    * Converts a list of tuples (String, Any) into a JsonObject.
    */
-  def mapToJsonObject(pairs: (String, Any)*): JsonObject = mapToJsonObject(pairs.toMap)
+  def jsonObject(pairs: (String, Any)*): JsonObject = mapToJsonObject(pairs.toMap)
 
   /* implicit converters */
 
@@ -106,7 +109,7 @@ trait ValueUtils {
   implicit def valueToString(v: Value): String = v.getString
   implicit def valueToMap(v: Value): Map[String, _] = jsonObjectToMap(v.getMap)
   implicit def valueToList(v: Value): List[_] = jsonArrayToList(v.getArray)
-  
+
   implicit def booleanToValue(x: Boolean) = new Value(x)
   implicit def numberToValue(x: Number) = new Value(x)
   implicit def intToValue(x: Int) = numberToValue(x)
@@ -116,7 +119,7 @@ trait ValueUtils {
   implicit def binaryToValue(x: Array[Byte]) = new Value(x)
   implicit def stringToValue(x: String) = new Value(x)
   implicit def mapToValue(x: Map[String, _]) = new Value(mapToJsonObject(x))
-  implicit def listToValue(x: List[_]) = new Value(listToJsonArray(x))
+  implicit def listToValue(x: Seq[_]) = new Value(listToJsonArray(x))
 
   /**
    * Resolves the value with unknown type.
